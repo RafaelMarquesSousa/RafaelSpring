caae88b5cbb03d482429aa28c519f13f0113aaef
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-03-21 08:11:04 -0300

    t

---
+++ Rafael-1/src/main/java/com/Rafael/com/Rafael1Application.java
@@ -0,0 +1,13 @@
+package com.Rafael.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rafael1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rafael1Application.class, args);
+	}
+
+}
---
+++ Rafael-1/src/test/java/com/Rafael/com/Rafael1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rafael.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rafael1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
